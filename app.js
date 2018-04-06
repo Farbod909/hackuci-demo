9d2f6dec309eb446e0ba7d08725eed7a981fbfe8
Farbod909 <rafezyfarbod@gmail.com>
2018-04-05 21:40:14 -0700

    Allow CORS and log message on server start

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
+const port = 8080
+
 app.use(bodyParser.urlencoded({ extended: true }))
 db.defaults({ emails: [] }).write()
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/api/addEmail', (req, res) => 
 {
@@ -23,7 +31,7 @@ app.post('/api/addEmail', (req, res) =>
 	else 
 	{
 		db.get('emails').push(email).write()
-		res.status(200).end()
+		res.status(200).send("Success").end()
 	}
 })
 
@@ -32,4 +40,7 @@ app.get('/api/getEmails', (req, res) =>
 	res.end(db.get('emails').value().join(', '))
 })
 
-app.listen(8080)
+app.listen(port, () => 
+{
+	console.log("Server started on port " + port)
+})
